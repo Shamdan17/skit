ebd9200a33cd98a024ebdaaf11bfd701ef1f401b
Shadi Hamdan <shamdan17@ku.edu.tr>
2023-11-28 18:20:19 +0300

    Improve memory printing

--- skit/timers.py
+++ skit/timers.py
@@ -30,27 +30,41 @@ class BlockTimer:
 class Ticker:
     """
     A simple timer class that prints the time elapsed since the last tick.
+
     Usage:
     > t = Ticker()
     > t("Part A")
-    Part A took: 1.63 ms
-    > t("Part B")
-    Part B took: 45.61 ms
-    ...
-    > t.reset()
+    Tick Part A took: 1.63 ms
     > t()
-    Tick took: 3.09 ms
+    Tick took: 45.61 ms
+    ...
+
+    Methods:
+    - __init__(self, verbose=True): Initializes the Timer object.
+    - reset(self): Resets the timer by updating the start time to the current time.
+    - tick(self, name=None): Prints the time elapsed since the last tick.
+    - __call__(self, name=None): Prints the time elapsed since the last tick.
+
+    Attributes:
+    - verbose (bool): Whether to print verbose output or not. Default is True.
     """
 
-    def __init__(self):
+    def __init__(self, verbose=True):
+        """
+        Initialize the Timer object.
+
+        Parameters:
+        - verbose (bool): Whether to print verbose output or not. Default is True. If false, only the
+        time elapsed since the last tick will be returned without printing.
+        """
         self.reset()
+        self.verbose = verbose
 
     def reset(self):
         """
-        Resets the timer.
-        Usage:
-        > t = Ticker()
-        > t.reset()
+        Resets the timer by updating the start time to the current time.
+
+        Call this function if you want to reset the timer without printing
         """
         self.start = timeit.default_timer()
 
@@ -58,28 +72,44 @@ class Ticker:
         """
         Prints the time elapsed since the last tick. You can optionally
         provide a name for the tick. The same function can also be called
-        directly
+        directly.
+
         Usage:
         > t = Ticker()
         > t.tick("Part A")
         Tick Part A took: 1.63 ms
         > t()
         Tick took: 45.61 ms
+
+        Parameters:
+        - name (str): The name for the tick. Default is None.
+
+        Returns:
+        - tick_time (float): The time elapsed since the last tick in milliseconds.
         """
         if name is None:
-            name = "Tick"
+            name = ""
         tick_time = (timeit.default_timer() - self.start) * 1000.0
-        print("{} took: {:.2f} ms.".format(name, tick_time))
+        if self.verbose:
+            print("Tick {} took: {:.2f} ms.".format(name, tick_time))
         self.start = timeit.default_timer()
+        return tick_time
 
     def __call__(self, name=None):
         """
         Prints the time elapsed since the last tick.
+
         Usage:
         > t = Ticker()
         > t("Part A")
         Tick Part A took: 1.63 ms
         > t()
         Tick took: 45.61 ms
+
+        Parameters:
+        - name (str): The name for the tick. Default is None.
+
+        Returns:
+        - tick_time (float): The time elapsed since the last tick in milliseconds.
         """
-        self.tick(name)
+        return self.tick(name)
